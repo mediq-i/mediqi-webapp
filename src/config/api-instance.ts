e9241d49cbe6073d01d12ffee4523661814c64c5
Charles Ilo <charlesikilo@gmail.com>
2025-02-02 00:20:03 +0100

    sign up api integration

--- src/config/api-instance.ts
+++ src/config/api-instance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const apiInstance = axios.create({
-  baseURL: "https://mediqi-backend.onrender.com/v1/api",
-});
 // const apiInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+//   baseURL: "https://mediqi-backend.onrender.com/v1/api",
 // });
+const apiInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+});
 
 export default apiInstance;
